280d2bce793a3ed1f4d6e313d424c03f09dd0e2e
chakb <emiliosdh@gmail.com>
2021-12-27 00:56:21 +0100

    exercises 2.6 - 2.10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ onSubmit, name, handleNameChange, number, handleNumberChange }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={name} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={number} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Person = ({ person }) => {
+  return (
+    <li>{person.name} {person.number}</li>
+  )
+}
+
+const Persons = ({ persons }) => {
+  return (
+    <ul>
+      {persons.map(person => <Person key={person.name} person={person} />)}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some((person) => person.name === newName)) {
+      return window.alert(`${newName} is already added to phonebook`)
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+      />
+      <h2>Add new</h2>
+      <PersonForm
+        onSubmit={addPerson}
+        name={newName}
+        handleNameChange={handleNameChange}
+        number={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
