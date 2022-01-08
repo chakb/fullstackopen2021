3dce8fce75b9b0e6230045d6df25368c87b4ac00
chakb <emiliosdh@gmail.com>
2022-01-09 00:06:14 +0100

    exercises 3.1 - 3.22

---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: v => /(\D*\d){8,}/.test(v),
+      message: props => `${props.value} doesn't have at least 8 digits`
+    },
+    required: true
+  }
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+personSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Person', personSchema)
---
+++ part3/phonebook-frontend/src/App.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ onSubmit, name, handleNameChange, number, handleNumberChange }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={name} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={number} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Person = ({ person, handleDeletePerson }) => {
+  return (
+    <li>
+      {person.name} {person.number} <button value={person.id} onClick={handleDeletePerson}>delete</button>
+    </li>
+  )
+}
+
+const Persons = ({ persons, handleDeletePerson }) => {
+  return (
+    <ul>
+      {persons.map(person => <Person key={person.name} person={person} handleDeletePerson={handleDeletePerson} />
+      )}
+    </ul>
+  )
+}
+
+const Notification = ({ message }) => {
+  if (message.text === null) {
+    return null
+  }
+
+  return (
+    <div className={message.className}>
+      {message.text}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState({ text: null, className: null })
+
+  useEffect(() => {
+    personService.getAll()
+      .then(initialPersons => setPersons(initialPersons))
+  }, [])
+
+  const notifyWith = (text, className) => {
+    setNotificationMessage({ text, className })
+    setTimeout(() => {
+      setNotificationMessage({ ...notificationMessage, text: null })
+    }, 5000)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personFound = persons.find(person => person.name === newName)
+
+    if (personFound) {
+      if (window.confirm(`${personFound.name} is already added to phonebook, replace old number with a new one?`)) {
+        const changedPerson = { ...personFound, number: newNumber }
+        personService
+          .update(personFound.id, changedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id === personFound.id ? returnedPerson : person))
+          })
+          .catch(error => {
+            notifyWith(error.response.data.error, 'error')
+          })
+      }
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+        .then(() => {
+          notifyWith(`Added ${personObject.name}`, 'success')
+        })
+        .catch(error => {
+          notifyWith(error.response.data.error, 'error')
+        })
+    }
+  }
+
+  const handleDeletePerson = (event) => {
+    const idToDelete = event.target.value
+    const personToDelete = persons.find(person => person.id === idToDelete)
+    const personsRemaining = persons.filter(person => person.id !== idToDelete)
+
+    if (window.confirm(`Delete ${personToDelete.name}?`)) {
+      personService
+        .remove(event.target.value)
+        .then(() =>
+          setPersons(personsRemaining))
+        .catch(() => {
+          notifyWith(`Information of ${personToDelete.name} has already been removed from the server`, 'error')
+          setPersons(personsRemaining)
+        })
+    }
+  }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Notification message={notificationMessage} />
+      <Filter
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+      />
+      <h2>Add new</h2>
+      <PersonForm
+        onSubmit={addPerson}
+        name={newName}
+        handleNameChange={handleNameChange}
+        number={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} handleDeletePerson={handleDeletePerson} />
+    </div>
+  )
+}
+
+export default App
